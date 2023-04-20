a98300ffa778ef5cbd396c8964f32f73d286e4d5
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-20 16:42:05 +0300

    create env variables

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&_11ze))g-%wpj^d36^gujw*fej+k&a63&3f5!3+c_^&+-jcx7'
+SECRET_KEY = os.getenv('SECRET_KET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['webserver', '127.0.0.1']
 
