57f45824ba4df3b98f76674b1e22cfb50ff835c6
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-20 16:38:09 +0300

    add localhosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&_11ze))g-%wpj^d36^gujw*fej+k&a63&3f5!3+c_^&+-jcx7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webserver', '127.0.0.1']
 
 
 # Application definition
