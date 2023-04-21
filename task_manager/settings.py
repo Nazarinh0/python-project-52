ee75501c7681ae3fe1a755ebbdf9a7ebec124c9e
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-21 15:10:55 +0300

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KET')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
