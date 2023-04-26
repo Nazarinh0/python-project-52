90aa915565c48550a4e6289fed441f2f11e8ed1f
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-26 22:36:49 +0300

    add i18n

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -116,6 +116,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = (
+    ('en-us', 'English'),
+    ('ru-ru', 'Russian'),
+)
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
