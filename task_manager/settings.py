2b371027836eeeeda26df9eae31f475bbd958830
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-27 21:14:51 +0300

    initialize users app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
+    'task_manager.users',
     'bootstrap5',
 ]
 
@@ -104,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
