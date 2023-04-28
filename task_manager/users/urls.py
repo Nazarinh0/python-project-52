bd37e75a24ab58b90b48be9c62a2aac5fa376216
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-28 23:00:33 +0300

    add user deleting view, template and route

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserCreateView, UserIndexView, UserUpdateView
+from .views import UserCreateView, UserIndexView, UserUpdateView, UserDeleteView
 
 
 urlpatterns = [
     path('', UserIndexView.as_view(), name='user_index'),
     path('create/', UserCreateView.as_view(), name='sign_up'),
-    path('<int:pk>/update', UserUpdateView.as_view(), name='user_update')
+    path('<int:pk>/update', UserUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete', UserDeleteView.as_view(), name='user_delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
-from task_manager.users.forms import UserForm
+from task_manager.users.forms import UserForm, UserUpdateForm
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
@@ -14,8 +14,10 @@ User = get_user_model()
 class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserForm
     template_name = 'form.html'
+
     success_url = reverse_lazy('login')
     success_message = _('User created successfully')
+
     extra_context = {
         'title': _('Registration'),
         'button_text': _('Register'),
@@ -30,13 +32,36 @@ class UserIndexView(ListView):
 
 class UserUpdateView(UserCheckMixin, SuccessMessageMixin, UpdateView):
     model = User
+    form_class = UserUpdateForm
     template_name = 'form.html'
+
     permission_message = _("You can't update other users")
     no_login_message = _('You are not logged in! Please log in')
     permission_url = reverse_lazy("user_index")
+
     success_message = _('User is successfully edited')
     success_url = reverse_lazy('user_index')
+
     extra_context = {
         'title': _('Editing user'),
         'button_text': _('Submit changes'),
+    }
+
+
+class UserDeleteView(SuccessMessageMixin, DeleteView):
+    model = User
+    template_name = 'users/delete.html'
+
+    permission_message = _("You can't delete other users")
+    no_login_message = _('You are not logged in! Please log in')
+    permission_url = reverse_lazy("user_index")
+    protected_message = _("User can't be deleted because he have assigned tasks")
+    protected_url = reverse_lazy('users')
+
+    success_url = reverse_lazy('user_index')
+    success_message = _('User is successfully deleted')
+
+    extra_context = {
+        'title': _('Deleting user'),
+        'button_text': _('Yes, delete'),
     }
