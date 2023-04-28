9f0c2fe53c56cb31aecb8f2375504be1cf890835
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-28 19:50:32 +0300

    add update route and fixed other routes in urls.py

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserCreateView
+from .views import UserCreateView, UserIndexView, UserUpdateView
 
 
 urlpatterns = [
+    path('', UserIndexView.as_view(), name='user_index'),
     path('create/', UserCreateView.as_view(), name='sign_up'),
+    path('<int:pk>/update', UserUpdateView.as_view(), name='user_update')
 ]
