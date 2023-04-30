40c410972cc391020abfcf7de931983c87e97ed4
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-30 14:22:49 +0300

    register status model

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from task_manager.users.models import User
+from task_manager.statuses.models import Status
 
 
 admin.site.register(User)
+admin.site.register(Status)
