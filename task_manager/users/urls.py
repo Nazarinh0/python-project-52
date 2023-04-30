3a4a42f80befb801d3ac5cbbb5584cbbf86f1e65
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-30 12:59:52 +0300

    small fix in route name

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserCreateView, UserIndexView, UserUpdateView, UserDeleteView
+from .views import UserCreateView, UsersIndexView, UserUpdateView, UserDeleteView
 
 
 urlpatterns = [
-    path('', UserIndexView.as_view(), name='user_index'),
+    path('', UsersIndexView.as_view(), name='users_index'),
     path('create/', UserCreateView.as_view(), name='sign_up'),
     path('<int:pk>/update', UserUpdateView.as_view(), name='user_update'),
     path('<int:pk>/delete', UserDeleteView.as_view(), name='user_delete'),
