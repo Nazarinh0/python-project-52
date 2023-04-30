8699ccb17d4e9a0301fca5d0843da7f648090551
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-30 12:14:32 +0300

    add deleteprotection in mixins.py

--- task_manager/users/mixins.py
+++ task_manager/users/mixins.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.db.models import ProtectedError
 from django.utils.translation import gettext_lazy as _
 
 
@@ -19,3 +20,15 @@ class UserCheckMixin(LoginRequiredMixin, UserPassesTestMixin):
         else:
             messages.error(self.request, self.no_login_message)
         return redirect(self.success_url)
+
+
+class DeleteProtectionMixin:
+    protected_message = None
+    protected_url = None
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(request, self.protected_message)
+            return redirect(self.protected_url)
