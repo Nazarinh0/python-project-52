bd73b7c00030b9c98cb467c460ca5dadf19ee846
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-04-30 14:27:28 +0300

    add status model and apply migrations

---
+++ task_manager/statuses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Status(models.Model):
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Creation Date'),
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Status')
+        verbose_name_plural = _('Statuses')
