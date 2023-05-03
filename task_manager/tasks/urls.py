fa9eaac9dac99953b348c45d6e658ed9709b8a0c
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-03 21:54:01 +0300

    add content to tasks/urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-
+from task_manager.tasks.views import TasksIndexView, TaskCreateView, TaskDetailView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
-    # path('', IndexView.as_view(), name='tasks_index'),
+    path('', TasksIndexView.as_view(), name='tasks_index'),
+    path('<int:pk>/', TaskDetailView.as_view(), name='task_show'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
+    path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
     ]
