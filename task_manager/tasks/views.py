0e0748e39116993427f5f430458be4e2f468920e
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-03 21:54:19 +0300

    add delete view in tasks/views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from task_manager.mixins import DeleteProtectionMixin
+from task_manager.mixins import DeleteAuthorCheckMixin
 from task_manager.users.models import User
 from .models import Task
 from .forms import TaskForm
 
 
 class TasksIndexView(LoginRequiredMixin, ListView):
-    model = Status
+    model = Task
     template_name = 'tasks/index.html'
 
     context_object_name = 'tasks'
@@ -21,6 +21,7 @@ class TasksIndexView(LoginRequiredMixin, ListView):
 class TaskDetailView(LoginRequiredMixin, DetailView):
     template_name = 'tasks/show.html'
     model = Task
+
     context_object_name = 'task'
     extra_context = {
         'title': _('Task details')
@@ -44,7 +45,7 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
         Set current user as the task's author.
         """
         user = self.request.user
-        form.instance.author = User.objects.get(pk=user.pk)
+        form.instance.author = user
         return super().form_valid(form)
 
 
@@ -62,7 +63,7 @@ class TaskUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     }
 
 
-class TaskDeleteView(LoginRequiredMixin, DeleteProtectionMixin,
+class TaskDeleteView(LoginRequiredMixin, DeleteAuthorCheckMixin,
                        SuccessMessageMixin, DeleteView):
     model = Task
     template_name = 'tasks/delete.html'
@@ -70,8 +71,8 @@ class TaskDeleteView(LoginRequiredMixin, DeleteProtectionMixin,
     success_url = reverse_lazy('tasks_index')
     success_message = _('Task deleted successfully')
 
-    protected_message = _("The task can be deleted only by its author")
-    protected_url = reverse_lazy('tasks_index')
+    permission_message = _("Task can be deleted only by its author")
+    permission_url = reverse_lazy('tasks_index')
 
     extra_context = {
         'title': _('Deleting task'),
