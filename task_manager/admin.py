fb82359087aa952bdd502d8bfa2b89a454b80178
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-04 14:10:36 +0300

    register Label model in admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from task_manager.users.models import User
 from task_manager.statuses.models import Status
-
+from task_manager.labels.models import Label
 
 admin.site.register(User)
 admin.site.register(Status)
+admin.site.register(Label)
