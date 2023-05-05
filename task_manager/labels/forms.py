61045fa2dad88b4a64b94a7643fb96ba9b3e76ac
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-05 15:33:43 +0300

    add new translations

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import gettext as _
 from .models import Label
 
 
@@ -7,6 +7,7 @@ class LabelForm(forms.ModelForm):
     name = forms.CharField(
         max_length=150,
         required=True,
+        label=_('Name')
     )
 
     class Meta:
--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
 class Label(models.Model):
     name = models.CharField(
         max_length=150,
         unique=True,
         blank=False,
+        verbose_name=_('Name'),
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
+        verbose_name=_('Creation date'),
     )
 
     def __str__(self):
