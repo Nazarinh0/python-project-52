4f4ae39893fea2c11b621f523c33f29e97f2abe8
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-12 16:35:15 +0300

    add dj-database-url usage in settings.py and add allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,7 +28,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1']
+ALLOWED_HOSTS = ['webserver',
+                 '127.0.0.1',
+                 '0.0.0.0',
+                 'localhost',
+                 '.railway.app']
 
 
 # Application definition
@@ -85,10 +90,10 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+    )
 }
 
 
