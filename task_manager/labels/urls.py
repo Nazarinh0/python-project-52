e5187f569abc6d307085d719b2d6874cdff8e2eb
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-15 23:59:45 +0300

    small fixes due to linter suggestions

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('create/', LabelCreateView.as_view(), name='label_create'),
     path('<int:pk>/update/', LabelUpdateView.as_view(), name='label_update'),
     path('<int:pk>/delete/', LabelDeleteView.as_view(), name='label_delete'),
-    ]
+]
--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('create/', TaskCreateView.as_view(), name='task_create'),
     path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    ]
+]
