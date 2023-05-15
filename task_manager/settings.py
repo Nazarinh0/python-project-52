6b4ca40e564ef5490bfdda6b714a2c49565c9536
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-15 21:48:35 +0300

    switch language to ru in setttings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -122,7 +122,7 @@ AUTH_USER_MODEL = "users.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
