a8b5a0de7da088d37a4a0095714bae569c40bf35
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-15 21:00:32 +0300

    fix in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -146,8 +146,11 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
