07537fe3b222881b33f4f5acbec540fa8ad00ea0
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-15 23:32:02 +0300

    change task model field name

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -29,7 +29,7 @@ class Task(models.Model):
         related_name='status',
         verbose_name=_('status')
     )
-    executor = models.ForeignKey(
+    assignee = models.ForeignKey(
         User,
         blank=True,
         on_delete=models.PROTECT,
