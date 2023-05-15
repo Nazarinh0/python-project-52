278668e397f594145d8dc7af130bc9bd0b7b58d2
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-05-15 19:52:00 +0300

    fix in users/urls.py

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -6,6 +6,6 @@ from .views import UserCreateView, UsersIndexView, \
 urlpatterns = [
     path('', UsersIndexView.as_view(), name='users_index'),
     path('create/', UserCreateView.as_view(), name='sign_up'),
-    path('<int:pk>/update', UserUpdateView.as_view(), name='user_update'),
-    path('<int:pk>/delete', UserDeleteView.as_view(), name='user_delete'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
