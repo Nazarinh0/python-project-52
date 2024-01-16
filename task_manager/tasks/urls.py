2f0c59d862d504cbbb4da7f03d04a25b09e33e6c
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-01-16 21:59:47 +0500

    codestyle fix in urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from task_manager.tasks.views import TasksIndexView, TaskCreateView, \
     TaskDetailView, TaskUpdateView, TaskDeleteView
 
+
 urlpatterns = [
     path('', TasksIndexView.as_view(), name='tasks_index'),
     path('<int:pk>/', TaskDetailView.as_view(), name='task_show'),
